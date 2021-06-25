f33e24250e2e11c167340490f28ce22aa436c53b
LLin2021 <LLin2021@admin.com>
2021-06-25 15:46:24 +0800

    项目部署完成  nginx 代理完成

--- dj31/wsgi.py
+++ dj31/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj31.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj31.settings.prod')
 
 application = get_wsgi_application()
